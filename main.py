fd34ba591b4adf5ef96af99b1d0a33659c5c81a7
Alan Gaines <122575215+arg0070aub@users.noreply.github.com>
2023-10-10 08:40:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,8 @@ from util import Util
 from iac_testing_antipatterns import IaCTestingAntipatterns
 
 def main():
-    
-    
-    
     base_dir= input("Please enter the directory: ")
     print("\n")
-    
     
     project_name = input ("Please enter the project name: ")
     print(project_name)
@@ -26,8 +22,8 @@ def main():
 #    quit()
     
     iac_testing_antipatterns = IaCTestingAntipatterns(files, project_name)
-#    
+
     iac_testing_antipatterns.get_anti_pattern_list()
-#    
+
 if __name__ == "__main__":
     main()
